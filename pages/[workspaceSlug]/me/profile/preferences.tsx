563eec5c405a1d6fd039832185b8d3ed8c1e8d4f
Irvan Mlambo <mlamboirvin8@gmail.com>
2023-09-05 21:07:34 +0200

    Initial commit

---
+++ pages/[workspaceSlug]/me/profile/preferences.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+
+// hooks
+import useUserAuth from "hooks/use-user-auth";
+// layouts
+import { WorkspaceAuthorizationLayout } from "layouts/auth-layout";
+import SettingsNavbar from "layouts/settings-navbar";
+// components
+import { CustomThemeSelector, ThemeSwitch } from "components/core";
+// ui
+import { Spinner } from "components/ui";
+import { BreadcrumbItem, Breadcrumbs } from "components/breadcrumbs";
+// types
+import { ICustomTheme } from "types";
+
+const ProfilePreferences = () => {
+  const { user: myProfile } = useUserAuth();
+  const { theme } = useTheme();
+  const [customThemeSelectorOptions, setCustomThemeSelectorOptions] = useState(false);
+  const [preLoadedData, setPreLoadedData] = useState<ICustomTheme | null>(null);
+
+  useEffect(() => {
+    if (theme === "custom") {
+      if (myProfile?.theme.palette)
+        setPreLoadedData({
+          background: myProfile.theme.background !== "" ? myProfile.theme.background : "#0d101b",
+          text: myProfile.theme.text !== "" ? myProfile.theme.text : "#c5c5c5",
+          primary: myProfile.theme.primary !== "" ? myProfile.theme.primary : "#3f76ff",
+          sidebarBackground:
+            myProfile.theme.sidebarBackground !== ""
+              ? myProfile.theme.sidebarBackground
+              : "#0d101b",
+          sidebarText: myProfile.theme.sidebarText !== "" ? myProfile.theme.sidebarText : "#c5c5c5",
+          darkPalette: false,
+          palette:
+            myProfile.theme.palette !== ",,,,"
+              ? myProfile.theme.palette
+              : "#0d101b,#c5c5c5,#3f76ff,#0d101b,#c5c5c5",
+        });
+      if (!customThemeSelectorOptions) setCustomThemeSelectorOptions(true);
+    }
+  }, [myProfile, theme, customThemeSelectorOptions]);
+
+  return (
+    <WorkspaceAuthorizationLayout
+      breadcrumbs={
+        <Breadcrumbs>
+          <BreadcrumbItem title="My Profile Preferences" />
+        </Breadcrumbs>
+      }
+    >
+      {myProfile ? (
+        <div className="p-8">
+          <div className="mb-8 space-y-6">
+            <div>
+              <h3 className="text-3xl font-semibold">Profile Settings</h3>
+              <p className="mt-1 text-custom-text-200">
+                This information will be visible to only you.
+              </p>
+            </div>
+            <SettingsNavbar profilePage />
+          </div>
+          <div className="space-y-8 sm:space-y-12">
+            <div className="grid grid-cols-12 gap-4 sm:gap-16">
+              <div className="col-span-12 sm:col-span-6">
+                <h4 className="text-lg font-semibold text-custom-text-100">Theme</h4>
+                <p className="text-sm text-custom-text-200">
+                  Select or customize your interface color scheme.
+                </p>
+              </div>
+              <div className="col-span-12 sm:col-span-6">
+                <ThemeSwitch
+                  user={myProfile}
+                  setPreLoadedData={setPreLoadedData}
+                  customThemeSelectorOptions={customThemeSelectorOptions}
+                  setCustomThemeSelectorOptions={setCustomThemeSelectorOptions}
+                />
+              </div>
+            </div>
+            {customThemeSelectorOptions && <CustomThemeSelector preLoadedData={preLoadedData} />}
+          </div>
+        </div>
+      ) : (
+        <div className="grid h-full w-full place-items-center px-4 sm:px-0">
+          <Spinner />
+        </div>
+      )}
+    </WorkspaceAuthorizationLayout>
+  );
+};
+
+export default ProfilePreferences;
---
+++ pages/[workspaceSlug]/projects/[projectId]/settings/integrations.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+import { useRouter } from "next/router";
+
+import useSWR from "swr";
+
+// layouts
+import { ProjectAuthorizationWrapper } from "layouts/auth-layout";
+// services
+import IntegrationService from "services/integration";
+import projectService from "services/project.service";
+// components
+import { SettingsHeader, SingleIntegration } from "components/project";
+// ui
+import { EmptyState, IntegrationAndImportExportBanner, Loader } from "components/ui";
+import { BreadcrumbItem, Breadcrumbs } from "components/breadcrumbs";
+// icons
+import { PlusIcon, PuzzlePieceIcon } from "@heroicons/react/24/outline";
+// images
+import emptyIntegration from "public/empty-state/integration.svg";
+// types
+import { IProject } from "types";
+import type { NextPage } from "next";
+// fetch-keys
+import { PROJECT_DETAILS, WORKSPACE_INTEGRATIONS } from "constants/fetch-keys";
+
+const ProjectIntegrations: NextPage = () => {
+  const router = useRouter();
+  const { workspaceSlug, projectId } = router.query;
+
+  const { data: projectDetails } = useSWR<IProject>(
+    workspaceSlug && projectId ? PROJECT_DETAILS(projectId as string) : null,
+    workspaceSlug && projectId
+      ? () => projectService.getProject(workspaceSlug as string, projectId as string)
+      : null
+  );
+
+  const { data: workspaceIntegrations } = useSWR(
+    workspaceSlug ? WORKSPACE_INTEGRATIONS(workspaceSlug as string) : null,
+    () =>
+      workspaceSlug
+        ? IntegrationService.getWorkspaceIntegrationsList(workspaceSlug as string)
+        : null
+  );
+
+  return (
+    <ProjectAuthorizationWrapper
+      breadcrumbs={
+        <Breadcrumbs>
+          <BreadcrumbItem
+            title={`${projectDetails?.name ?? "Project"}`}
+            link={`/${workspaceSlug}/projects/${projectId}/issues`}
+          />
+          <BreadcrumbItem title="Integrations" />
+        </Breadcrumbs>
+      }
+    >
+      <div className="h-full flex flex-col p-8 overflow-hidden">
+        <SettingsHeader />
+        {workspaceIntegrations ? (
+          workspaceIntegrations.length > 0 ? (
+            <section className="space-y-8 overflow-y-auto">
+              <IntegrationAndImportExportBanner bannerName="Integrations" />
+              <div className="space-y-5">
+                {workspaceIntegrations.map((integration) => (
+                  <SingleIntegration
+                    key={integration.integration_detail.id}
+                    integration={integration}
+                  />
+                ))}
+              </div>
+            </section>
+          ) : (
+            <EmptyState
+              title="You haven't configured integrations"
+              description="Configure GitHub and other integrations to sync your project resolutions."
+              image={emptyIntegration}
+              buttonText="Configure now"
+              onClick={() => router.push(`/${workspaceSlug}/settings/integrations`)}
+            />
+          )
+        ) : (
+          <Loader className="space-y-5">
+            <Loader.Item height="40px" />
+            <Loader.Item height="40px" />
+            <Loader.Item height="40px" />
+            <Loader.Item height="40px" />
+          </Loader>
+        )}
+      </div>
+    </ProjectAuthorizationWrapper>
+  );
+};
+
+export default ProjectIntegrations;
